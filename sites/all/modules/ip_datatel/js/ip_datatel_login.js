31960a850fac3dd6a5df72d3cca328797d95ca68
Fortified <colton.echols@fortifiedsecurityservices.com>
2013-08-12 17:49:55 -0700

    8-12-13

--- sites/all/modules/ip_datatel/js/ip_datatel_login.js
+++ sites/all/modules/ip_datatel/js/ip_datatel_login.js
@@ -1,6 +1,7 @@
 (function($){
 	$(document).ready(function(){
 		$('.alarm_login').click(function(){
+			$('.alarm-modal').html('');
 			var alarmIframe = '<iframe seamless width="100%" height="700px" src="http://fortifiedss.alarmdealer.com/index.php?mod=auth&action=login"></iframe>';
 			$('.alarm-modal').append(alarmIframe);
 			$('#myAlarmModal').foundation('reveal', 'open');
