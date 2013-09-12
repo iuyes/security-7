7151e34b00e79e09f164ed5456d71e6548826b28
Fortified <colton.echols@fortifiedsecurityservices.com>
2013-09-12 14:14:04 -0700

    Fixes for customers cameras not revealing on dashboard

--- sites/all/modules/fr_user_dash/js/fr_user_dash_reveal.js
+++ sites/all/modules/fr_user_dash/js/fr_user_dash_reveal.js
@@ -4,7 +4,6 @@
 		$('.cust-dvr-modal').html('');
 		e.preventDefault();
 		var dvrId = $(this).attr('data-dvr-nid');
-		console.log(dvrId);
 		if(dvrId != ''){
 			var dvrUrl = $('#myDvrModal'+dvrId+'').attr('data-dvr-url');
 			var dvrIframe = '<iframe seamless width="100%" height="700px" src="http://'+dvrUrl+'"></iframe>';
