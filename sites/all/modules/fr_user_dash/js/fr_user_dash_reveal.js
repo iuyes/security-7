6594337ef3fc61417294d2727eb6af2192d634f2
Fortified <colton.echols@fortifiedsecurityservices.com>
2013-09-11 19:51:54 -0700

    Reveal customers cameras

--- sites/all/modules/fr_user_dash/js/fr_user_dash_reveal.js
+++ sites/all/modules/fr_user_dash/js/fr_user_dash_reveal.js
@@ -5,7 +5,7 @@
 		e.preventDefault();
 		var dvrId = $(this).attr('data-dvr-nid');
 		console.log(dvrId);
-		if(dvrId){
+		if(dvrId != ''){
 			var dvrUrl = $('#myDvrModal'+dvrId+'').attr('data-dvr-url');
 			var dvrIframe = '<iframe seamless width="100%" height="700px" src="http://'+dvrUrl+'"></iframe>';
 			$('#myDvrModal'+dvrId+'').foundation('reveal', 'open');
