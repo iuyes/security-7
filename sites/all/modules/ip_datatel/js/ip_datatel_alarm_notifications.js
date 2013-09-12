8a6de71cb9c76d6e7c5b10cb03ebb6c6ac40482c
Fortified <colton.echols@fortifiedsecurityservices.com>
2013-09-11 19:34:15 -0700

    Updates for alarm processing and custom tooltips

--- sites/all/modules/ip_datatel/js/ip_datatel_alarm_notifications.js
+++ sites/all/modules/ip_datatel/js/ip_datatel_alarm_notifications.js
@@ -3,6 +3,7 @@
 		var stop = false;
 		var viewEvent;
 		var IpDatatel = new Object();
+		IpDatatel.functions = {};
 		IpDatatel.settings = Drupal.settings.ip_datatel;
 		if (IpDatatel.settings.processing_accounts && IpDatatel.settings.account_variables && IpDatatel.settings.uid) {
 			IpDatatel.settings.session = Math.floor(Math.random() * 101);
@@ -20,7 +21,6 @@
 					settings: IpDatatel.settings,
 				},
 				success: function(data) {
-				console.log(data.status);
 					if (data.status) {
 							IpDatatel.settings.start_time = data.status.start_time * 1;
 							IpDatatel.settings.new_events = data.status.new_events;
@@ -38,13 +38,25 @@
 			});
 		}
 		
-		$(document).on('click', '.view-event', function(){
-			$(this).parents('td').siblings().css('background', 'rgb(58, 209, 58)');
+		IpDatatel.functions.viewEventChangeColor = $(document).on('click', '.view-event', function(){
+			var tableRow = $(this).parents('td');
+			tableRow.css('background', 'rgb(58, 209, 58)').siblings().css('background', 'rgb(58, 209, 58)');
 			var clickedEvent = $(this).attr('data-arguments');
-			console.log(clickedEvent);
-			console.log(IpDatatel.settings.new_events[clickedEvent])
 			IpDatatel.settings.new_events[clickedEvent].clicked = 'TRUE';
+			IpDatatel.functions.updateNewEvents();
 		});
+		
+		IpDatatel.functions.removeEvent = $(document).on('click', '.remove-event', function(e){
+			e.preventDefault();
+			var eventId = $(this).attr('data-event');
+			$(this).html('Event will be removed in a few seconds!');
+			IpDatatel.settings.new_events[eventId].status = 'removed';
+			IpDatatel.functions.updateNewEvents();
+		});
+		
+		IpDatatel.functions.updateNewEvents = function(){
+			Drupal.settings.new_events = IpDatatel.settings.new_events;
+		}
 		
 	});
 })(jQuery);
